5832d8fc082e52eb2da216f40238bc2009597ab1
cassiojuniorr <cassiospcjunior@gmail.com>
2024-08-16 00:39:21 -0300

    style snackBar

--- lib/core/snack_bar.dart
+++ lib/core/snack_bar.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
-showSnackBar({
-  required BuildContext context,
-  required String text,
-  bool isErro = true
-}) {
+showSnackBar(
+    {required BuildContext context, required String text, bool isErro = true}) {
   final snackBar = SnackBar(
     content: Text(text),
+    backgroundColor: (isErro) ? Colors.red : Colors.green,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+    ),
   );
 
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
